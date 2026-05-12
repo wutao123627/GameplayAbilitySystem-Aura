3d04d020db1237c5c71f1e452d48c0f1dfd82eb1
wutao123627 <414846796@qq.com>
2026-05-12 15:59:24 +0800

    Aura HUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,14 @@
+// Copyright Druid Mechanics.
+
+
+#include "UI/HUD/AuraHUD.h"
+#include "UI/Widgets/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+
+}
