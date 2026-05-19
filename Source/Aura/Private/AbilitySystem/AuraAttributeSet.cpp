6e200eb9e50c004380395426c82c2a6b3db13039
wutao123627 <414846796@qq.com>
2026-05-19 22:04:58 +0800

    Modifier Order of Operations

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -9,7 +9,7 @@
 
 
 UAuraAttributeSet::UAuraAttributeSet() {
-	InitHealth(50.f);
+	InitHealth(10.f);
 	InitMaxHealth(100.f);
 	InitMana(10.f);
 	InitMaxMana(50.f);
@@ -35,10 +35,10 @@ void UAuraAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, 
 	Super::PreAttributeChange(Attribute,NewValue);
 
 	if (Attribute == GetHealthAttribute()) {
-		NewValue = FMath::Clamp(NewValue,0.f,GetMaxHealth());
+		//NewValue = FMath::Clamp(NewValue,0.f,GetMaxHealth());
 	}
 	if (Attribute == GetManaAttribute()) {
-		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxMana());
+		//NewValue = FMath::Clamp(NewValue, 0.f, GetMaxMana());
 	}
 }
 
@@ -78,10 +78,10 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallba
 	SetEffectProperties(Data, Props);
 
 	if (Data.EvaluatedData.Attribute == GetHealthAttribute()) {
-		SetHealth(FMath::Clamp(GetHealth(),0,GetMaxHealth()));
+		//SetHealth(FMath::Clamp(GetHealth(),0,GetMaxHealth()));
 	}
 	if (Data.EvaluatedData.Attribute == GetManaAttribute()) {
-		SetMana(FMath::Clamp(GetMana(), 0, GetMaxMana()));
+		//SetMana(FMath::Clamp(GetMana(), 0, GetMaxMana()));
 	}
 }
 
