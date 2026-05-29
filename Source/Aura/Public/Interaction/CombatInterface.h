3facff59c8bc76be2de9f8fd889f79b44969d4ae
wutao123627 <414846796@qq.com>
2026-05-29 23:20:33 +0800

    Spawning Projectiles

--- Source/Aura/Public/Interaction/CombatInterface.h
+++ Source/Aura/Public/Interaction/CombatInterface.h
@@ -6,7 +6,6 @@
 #include "UObject/Interface.h"
 #include "CombatInterface.generated.h"
 
-// This class does not need to be modified.
 UINTERFACE(MinimalAPI)
 class UCombatInterface : public UInterface
 {
@@ -20,7 +19,8 @@ class AURA_API ICombatInterface
 {
 	GENERATED_BODY()
 
-	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
 	virtual int32 GetPlayerLevel();
+	virtual FVector GetCombatSocketLocation();
+
 };
