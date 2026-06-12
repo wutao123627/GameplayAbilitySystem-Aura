2922c1e071d86c325014ab4db40c147023aa6ae2
wutao123627 <414846796@qq.com>
2026-06-12 23:08:31 +0800

    Find New Location Around Target

--- Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
+++ Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
@@ -20,7 +20,6 @@ void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, u
 	float ClosestDistance = TNumericLimits<float>::Max();
 	AActor* ClosestActor = nullptr;
 	for (AActor* Actor : ActorsWithTag) {
-		GEngine->AddOnScreenDebugMessage(-1,.5f,FColor::Orange,*Actor->GetName());
 
 		if (IsValid(Actor) && IsValid(OwningPawn)) {
 			const float Distance = OwningPawn->GetDistanceTo(Actor);
