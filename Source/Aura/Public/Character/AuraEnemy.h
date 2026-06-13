737fcef2a4709eafd2349365630f4ba8baf6cda3
wutao123627 <414846796@qq.com>
2026-06-14 01:46:40 +0800

    Combat Target

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -34,6 +34,9 @@ public:
 	*/
 	virtual int32 GetPlayerLevel() override;
 	virtual void Die() override;
+	virtual void SetCombatTarget_Implementation(AActor* InCombatTarget) override;
+	virtual AActor* GetCombatTarget_Implementation() const override;
+
 	/*end Combat Interface*/
 
 	UPROPERTY(BlueprintAssignable)
@@ -52,6 +55,9 @@ public:
 
 	UPROPERTY(EditAnywhere,BlueprintReadOnly, Category = "Combat")
 	float LifeSpan = 5.f;
+
+	UPROPERTY(BlueprintReadWrite,Category = "Combat")
+	TObjectPtr<AActor> CombatTarget;
 protected:
 	virtual void BeginPlay() override;
 	virtual void InitAbilityActorInfo() override;
