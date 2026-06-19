5ebdaf7a8ae441c57cc62be1e5675eef6c18fb7f
wutao123627 <414846796@qq.com>
2026-06-19 21:49:02 +0800

    Dead Blackboard Key

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -66,6 +66,7 @@ int32 AAuraEnemy::GetPlayerLevel()
 void AAuraEnemy::Die()
 {
 	SetLifeSpan(LifeSpan);
+	if(AuraAIController)AuraAIController->GetBlackboardComponent()->SetValueAsBool(FName("Dead"), true);
 	Super::Die();
 }
 
