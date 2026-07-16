70f6e800516f19deda72c8c04c588295c13c1751
wutao123627 <414846796@qq.com>
2026-07-16 22:42:23 +0800

    Showing Abilities in the Spell Tree

--- Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -39,6 +39,7 @@ void UAuraWidgetController::BroadcastAbilityInfo()
 		{
 			FAuraAbilityInfo Info = AbilityInfo->FindAbilityInfoForTag(AuraAbilitySystemComponent->GetAbilityTagFromSpec(AbilitySpec));
 			Info.InputTag = AuraAbilitySystemComponent->GetInputTagFromSpec(AbilitySpec);
+			Info.StatusTag = AuraAbilitySystemComponent->GetStatusFromSpec(AbilitySpec);
 			AbilityInfoDelegate.Broadcast(Info);
 		});
 	GetAuraASC()->ForEachAbility(BroadcastDelegate);
--- Source/Aura/Public/AbilitySystem/Data/AbilityInfo.h
+++ Source/Aura/Public/AbilitySystem/Data/AbilityInfo.h
@@ -17,6 +17,9 @@ struct FAuraAbilityInfo {
 	UPROPERTY( BlueprintReadOnly)
 	FGameplayTag InputTag = FGameplayTag();
 
+	UPROPERTY(BlueprintReadOnly)
+	FGameplayTag StatusTag = FGameplayTag();
+
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	FGameplayTag CooldownTag = FGameplayTag();
 
