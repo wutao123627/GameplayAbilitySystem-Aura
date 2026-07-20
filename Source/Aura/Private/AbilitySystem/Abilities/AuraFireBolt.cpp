b731794efa30ae1427066eae44179ad79ef5b46e
wutao123627 <414846796@qq.com>
2026-07-21 01:17:31 +0800

    Cost and Cooldown in Spell Description

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
@@ -0,0 +1,102 @@
+// Copyright Druid Mechanics.
+
+
+#include "AbilitySystem/Abilities/AuraFireBolt.h"
+#include "AuraGameplayTags.h"
+
+FString UAuraFireBolt::GetDescription(int32 Level)
+{
+	const int32 Damage = GetDamageByDamageType(Level,FAuraGameplayTags::Get().Damage_Fire);
+	const float ManaCost = FMath::Abs(GetManaCost(Level));
+	const float Cooldown = GetCooldown(Level);
+	if (Level == 1) {
+		return FString::Printf(
+			
+			TEXT(
+				//Title
+				"<Title>FIRE BOLT</>\n\n"
+
+				//Details
+				"<Small>Level: </><Level>%d</>\n"
+				//ManaCost
+				"<Small>ManaCost: </><ManaCost>%.1f</>\n"
+				//cooldown
+				"<Small>Cooldown: </><Cooldown>%.1f</>\n\n"
+
+				"<Default>Launched a bolt of fire, " 
+				"exploding on impact and dealing: </>"
+				
+				//Damage
+				"<Damage>%d</>"
+				"<Default> fire damage with a chance to burn</>"
+				),
+			//Value
+			Level,
+			ManaCost,
+			Cooldown,
+			Damage);
+	}
+	else {
+		return FString::Printf(
+			
+			TEXT(
+				//Title
+				"<Title>FIRE BOLT</>\n\n"
+
+				//Details
+				"<Small>Level: </><Level>%d</>\n"
+				//ManaCost
+				"<Small>ManaCost: </><ManaCost>%.1f</>\n"
+				//cooldown
+				"<Small>Cooldown: </><Cooldown>%.1f</>\n\n"
+
+				//Number of FireBolts
+				"<Default>Launched %d bolts of fire, " 
+				"exploding on impact and dealing: </>"
+				
+				//Damage
+				"<Damage>%d</>"
+				"<Default> fire damage with a chance to burn</>"
+				),
+			//Value
+			Level,
+			ManaCost,
+			Cooldown,
+			FMath::Min(Level, NumProjectiles),
+			Damage);
+	}
+	return FString();
+}
+
+FString UAuraFireBolt::GetNextLevelDescription(int32 Level)
+{
+	const int32 Damage = GetDamageByDamageType(Level, FAuraGameplayTags::Get().Damage_Fire);
+	const float ManaCost = FMath::Abs(GetManaCost(Level));
+	const float Cooldown = GetCooldown(Level);
+	return FString::Printf(
+		TEXT(
+			//Title
+			"<Title>NEXT LEVEL: </>\n\n"
+
+			//Details
+			"<Small>Level: </><Level>%d</>\n"
+			//ManaCost
+			"<Small>ManaCost: </><ManaCost>%.1f</>\n"
+			//cooldown
+			"<Small>Cooldown: </><Cooldown>%.1f</>\n\n"
+
+			//Number of FireBolts
+			"<Default>Launched %d bolts of fire, "
+			"exploding on impact and dealing: </>"
+
+			//Damage
+			"<Damage>%d</>"
+			"<Default> fire damage with a chance to burn</>"
+		),
+		//Value
+		Level,
+		ManaCost,
+		Cooldown,
+		FMath::Min(Level, NumProjectiles),
+		Damage);
+}
